ccd0c7ec91e3121bff60f9bdf1f506f908b4f900
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-10 15:57:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,36 @@
 # Add  code here!
+=begin
+def prime?(num)
+  n = 2
+  while n < num
+    return false if num % n == 0
+    n += 1
+  end
+  true
+end
+
+prime?(105557)
+=end
+
+=begin
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
+
+prime?(105557)
+=end
+
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
+end
+
+puts prime?(105557)
