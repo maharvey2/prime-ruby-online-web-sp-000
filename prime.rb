95d19044b986f51100d270c46af3d8ada6fe5690
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-10 15:32:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+=begin
+def prime?(num)
+  n = 2
+  while n < num
+    return false if num % n == 0 || -num % -n == 0
+    n += 1
+  end
+  true
+end
+
+prime?(-105557)
+=end
+
+
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
+
+prime?(105557)
